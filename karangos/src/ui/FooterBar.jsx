8e815ac6cbe8ce40c72238d8b23fe593eb521655
joao100101 <joaovitor20002@gmail.com>
2024-04-16 12:10:30 +0000

    (16/04) Correcao da prova e estilização do footerbar

--- karangos/src/ui/FooterBar.jsx
+++ karangos/src/ui/FooterBar.jsx
@@ -3,18 +3,28 @@ import {
     Toolbar,
     Typography
 } from '@mui/material'
-
+import CoffeeIcon from '@mui/icons-material/Coffee';
 const FooterBar = () => {
     return (
         <>
             <Toolbar
                 variant='dense'
                 component="footer"
+                sx={{
+                    position: 'fixed',
+                    bottom: 0,
+                    width: '100vw',
+                    justifyContent: 'center',
+                    backgroundColor: 'action.disabledBackground'
+                }}
             >
                 <Typography
                     variant='caption'
+                    sx={{
+                        '& a': { color: 'secondary.light' }
+                    }}
                 >
-                    Desenvolvido com café por <a href="mailto:joaovictormundel@gmail.com">João Victor Mundel</a>
+                    Desenvolvido com <CoffeeIcon fontSize='small'/> por <a href="mailto:joaovictormundel@gmail.com">João Victor Mundel</a>
                 </Typography>
             </Toolbar>
         </>
